ab521439fdd776e5195a8002c31df15b93dee5e8
Yonas <your@email.com>
2025-07-31 14:58:24 +0300

    Remove all files and folders

--- connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$dbhost = 'localhost';
-	$dbuser = 'root';
-	$dbpass = '';
-	$db = 'cms';
-	$conn = mysqli_connect($dbhost, $dbuser, $dbpass , $db) or die($conn);	
-?>
--- fill_sub_category.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//fill_sub_category.php
-
-include('database_connection.php');
-
-echo fill_select_box_two($connect, $_POST["id"]);
-
-?>
